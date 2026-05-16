7b650b8d98a16f9e6b9b009ba730b645c6dfb175
1adityatandon1234-netizen <adityastandon@gmail.com>
2026-05-16 15:44:54 -0500

    add swerve subsystem to robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,11 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.Kraken;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+
+import java.io.File;
+
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -18,6 +23,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+    private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
+      "swerve"));
   private final Kraken m_kraken = new Kraken();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
