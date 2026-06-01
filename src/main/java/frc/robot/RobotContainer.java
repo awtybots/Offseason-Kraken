18e14eb02d428a147c59d03d6df34c5781d8768c
1adityatandon1234-netizen <adityastandon@gmail.com>
2026-06-01 13:00:36 -0500

    CAS named command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -124,16 +124,21 @@ public class RobotContainer {
         NamedCommands.registerCommand("intake", m_intake.runIntakeCommand());
 
         // control all shooting
-        NamedCommands.registerCommand("Control All Shooting", Commands.defer(() -> {
-            ControlAllShooting shootCmd = new ControlAllShooting(
-                    m_shooter, m_conveyor, m_kicker, m_pushout, m_intake, m_hood, m_rollers, m_turret, drivebase);
-            return Commands.sequence(
+        NamedCommands.registerCommand("Control All Shooting",             
+            Commands.defer(() -> {
+                ControlAllShooting shootCmd = new ControlAllShooting(
+                  m_shooter, m_conveyor, m_kicker, m_pushout, m_intake, m_hood, m_rollers, m_turret, drivebase);
+                return Commands.sequence(
                  
-                    Commands.parallel(
-                            shootCmd,
-                            m_pushout.AgitateWhileIntakingCommand()))
-  ;
-        }, java.util.Collections.emptySet()).withTimeout(5.3));
+                  Commands.parallel(
+                    shootCmd,
+                    m_pushout.AgitateCommand()
+                  ).onlyWhile(() -> m_turret.isAtAngle() && m_hood.isAtAngle())
+                ).finallyDo(() -> {
+                  m_shooter.setTargetRPSCommand(shootCmd.recordedTargetRPS).withTimeout(1.0);
+                  });
+            }, java.util.Collections.emptySet())
+        );
 
         
 
