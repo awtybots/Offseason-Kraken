03835c243d0008bde8495ce3a7fa7a6286394270
salamander <asiersg@icloud.com>
2026-09-08 17:20:50 -0500

    limelight

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -111,7 +111,7 @@ public final class Constants {
   }
 
   public static class LimelightConstants {
-    public static final String LIMELIGHT_FRONT = "limelight-front";
+    public static final String LIMELIGHT_RIGHT = "limelight-right";  //10.58.29.15
     public static final String LIMELIGHT_BACK = "limelight-back";
     public static final String LIMELIGHT_LEFT = "limelight-left";
   }
